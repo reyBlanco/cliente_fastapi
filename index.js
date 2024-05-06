2c23d9a76ef7d3d11bcba0719195fc6f2e1a3ffe
Jesus Meza <jesus_meza108807@outlook.es>
2024-05-05 23:01:53 -0600

    construyendo formulario de caballeros del zodiaco,html y js, con estilo css

---
+++ index.js
@@ -0,0 +1,7 @@
+const d=document
+const $form_caballero=d.querySelector("#form_caballero"),
+      $text_caballero=$form_caballero.querySelector("#text_caballero"),
+      $text_constelacion=$form_caballero.querySelector("#text_constelacion"),
+      $text_dios=$form_caballero.querySelector("#text_dios");
+
+const $submit_enviar=$form_caballero.querySelector("#submit_enviar")
