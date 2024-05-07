6345022c06f1ce9bc55fed5704ed1dd2e92f8053
Jesus Meza <jesus_meza108807@outlook.es>
2024-05-07 15:19:41 -0600

    actualizando servidor

--- main.py.py
+++ main.py.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import status
-from base_model import *
 import sys
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Caballero_del_zodiaco(BaseModel):
+    nombre:Optional[str]
+    constelacion:Optional[str]
+    dios:Optional[str]
+
 
 
 app=FastAPI()
--- servidor_fastapi/base_model.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Caballero_del_zodiaco(BaseModel):
-    nombre:Optional[str]
-    constelacion:Optional[str]
-    dios:Optional[str]
