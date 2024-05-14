768a10d905765f63e964fdfe22eda6ff1a4e7f97
Jesus Meza <jesus_meza108807@outlook.es>
2024-05-13 19:49:45 -0600

    creando solicitar password

--- fetch.js
+++ fetch.js
@@ -8,18 +8,43 @@ const $submit_enviar=$form_caballero.querySelector("#submit_enviar");
 const $submit_websocket=d.querySelector("#submit_websocket");
 const $contenedor=d.querySelector("#contenedor");
 
+const $form_token=d.querySelector("#form_token"),
+      $text_password=$form_token.querySelector("#text_password"),
+      $submit_token=$form_token.querySelector("#submit_token"),
+      $contenedor_token=d.querySelector("#contenedor_token");
+      
+
 let url1="https://render-fbxh.onrender.com/enviar";
 let url2="http://192.168.100.16:3000/enviar"
+let url_ws="ws://192.168.100.16:3000/ws"
+let url_login="http://192.168.100.16:3000/login"
 
-let url_ws1="WSS://render-fbxh.onrender.com/ws"
-let url_ws2="ws://192.168.100.16:3000/ws"
+let url_render_login="https://render-fbxh.onrender.com/login"
+let url_render_ws="wss://render-fbxh.onrender.com/ws"
 
-const ws=new WebSocket(url_ws1)
+const ws=new WebSocket(url_render_ws)
+let form=new FormData()
 
 const enviar=async (paquete)=>{
     let {url,data,method,mode,headers,msgError}=paquete;
     let res=await fetch(url,{
         body:JSON.stringify(data),
+        headers,
+        method,
+        mode
+    });
+
+    if(res.ok){
+        return await res.json()
+    }else{
+        throw(msgError)
+    }
+}
+
+const enviar_password=async (paquete)=>{
+    let {url,data,method,mode,headers,msgError}=paquete;
+    let res=await fetch(url,{
+        body:data,
         headers,
         method,
         mode
@@ -79,6 +104,27 @@ d.addEventListener("click",(e)=>{
 
         ws.send(JSON.stringify(caballero))
     }
+    if(e.target===$submit_token){
+        //let {url,data,method,mode,headers,msgError}=paquete;
+        form.append("password",$text_password.value);
+        form.append("username","jesus")
+        let paquete={
+            url:url_render_login,
+            data:form,
+            method:"POST",
+            mode:"cors",
+            headers:{},
+            msgError:"error al mandar password"
+        }
+
+        enviar_password(paquete)
+        .then(res=>{
+            $contenedor_token.innerHTML=JSON.stringify(res)
+        })
+        .catch(err=>console.log(err))
+
+    }
+    
 });
 
 ws.addEventListener("open",(e)=>{
