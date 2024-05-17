033b30178312e0dcf05e2f101a7e44e4c334b654
Jesus Meza <jesus_meza108807@outlook.es>
2024-05-16 20:17:32 -0600

    corriegiendo puerto

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ async def wSocket(ws:WebSocket):
     await m_ws.escuchador_retransmision(ws)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
